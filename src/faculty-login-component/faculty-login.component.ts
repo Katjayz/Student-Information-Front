185450d603c0e2b9ffe8d72b786f2b1e2678c857
Katjayz <ashkal205@gmail.com>
2025-10-29 21:18:49 -0500

    Added VERY basic role functionality

--- src/faculty-login-component/faculty-login.component.ts
+++ src/faculty-login-component/faculty-login.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { RoleService } from '../role.service';
 
 //This is where the login page for faculty will go
 
@@ -12,12 +13,15 @@ import { Router, RouterModule } from '@angular/router';
 
 export class FacultyLoginComponent {
 
-    constructor(public router: Router) {}
+    constructor(public router: Router, public roleService: RoleService) {}
 
     
     loginVariable: string = "login please";
 
     navigate() {
-        this.router.navigate(['/example']);
+        this.roleService.studentLogin('john.smith@email.com','JohnsP@$$word')
+      .subscribe(response => {
+        this.roleService.saveToken(response.token);
+          });
     }
 }
---
+++ src/role.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+//This is the service used to check user roles. We can use it in guards
+//or to control what information is loaded on a page.
+export class RoleService{
+  
+  private loginUrl = 'http://localhost:8080/api/';
+  constructor(private http: HttpClient) {}
+  private tokenKey = 'role_token';
+
+  studentLogin(email: string, password: string) {
+    const params = new HttpParams()
+      .set('email', email)
+      .set('password', password);
+    return this.http.post<{token: string; role: string} >(this.loginUrl + "studentLogin", null, {params});
+  }
+
+  facultyLogin(email: string, password: string) {
+    const params = new HttpParams()
+      .set('email', email)
+      .set('password', password);
+    return this.http.post<{token: string; role: string} >(this.loginUrl + "facultyLogin", null, {params});
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  getRole(): string | null {
+    const token =localStorage.getItem(this.tokenKey);
+    if (!token){
+       return null;
+    }
+    const decoded: any = jwtDecode(token);
+    return decoded?.role||null;
+  }
+}
