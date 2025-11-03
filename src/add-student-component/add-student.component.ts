9d9851915e5dc1993fda0277a012908064a64ba8
Katjayz <ashkal205@gmail.com>
2025-11-02 18:38:38 -0600

    add student functionality

--- src/add-student-component/add-student.component.ts
+++ src/add-student-component/add-student.component.ts
@@ -3,18 +3,23 @@ import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+import { HttpClient, HttpParams } from '@angular/common/http';
+
 //This is where the student information access will go
 
 @Component({
   selector: 'app-add-student',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,],
   templateUrl: 'add-student.component.html'
 })
 
+
 export class AddStudentComponent {
   student = {
-    name: '',
+    id: 0,
+    firstName: '',
+    lastName: '',
     email: '',
     password: '',
     address: '',
@@ -24,12 +29,21 @@ export class AddStudentComponent {
     balance: 0
   };
 
-  constructor(private router: Router) {}
+  private apiUrl = 'http://localhost:8080/api/';
+
+  constructor(private router: Router, private http: HttpClient) {}
 
   onSubmit() {
-    console.log('Added Student:', this.student);
-    alert(`Student ${this.student.name} added!`);
-    this.router.navigate(['/student-list']);
+    this.http.post<boolean>(this.apiUrl + "addStudent", this.student).subscribe({
+    next: (response) => {
+      if (response == true) {
+        alert(`Student ${this.student.lastName} added!`);
+        this.router.navigate(['/student-list']);
+      } else {
+        alert(`Failed to add student ${this.student.lastName}`);
+      }
+      
+     }});
   }
 
   cancel() {
